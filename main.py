b8dae3eda5416cb46a2787065ec7965d948a9518
Arturo Meza <arturo97xd@gmail.com>
2024-05-10 04:37:13 -0500

    Add same name of .srt

--- main.py
+++ main.py
@@ -2,25 +2,23 @@ import faster_whisper
 import datetime
 from deep_translator import GoogleTranslator
 import os
-os.environ['KMP_DUPLICATE_LIB_OK']='True'   # Solución para el error de duplicación de librerías
+
+os.environ['KMP_DUPLICATE_LIB_OK']='True'  # Solución para el error de duplicación de librerías
 
 # Cargar el modelo de Whisper (puedes elegir el modelo según tus necesidades)
 model = faster_whisper.WhisperModel("base", device="cpu", compute_type="float32")
 
-# Especificar la ruta del archivo de video en tu sistema local
 video_file = "./09_Enumerations.mp4"
-
-# Transcribir el video directamente
 segments, _ = model.transcribe(video_file, beam_size=5, vad_filter=True)
 
-# Crear un objeto GoogleTranslator de deep_translator
+# Crear un objeto traductor de Google Translator para traducir los subtítulos al español o cualquier otro idioma cambiando el valor de target
 translator = GoogleTranslator(source='en', target='es')
 
-# Crear el contenido del archivo SRT
+
 srt_content = ""
 subtitle_count = 1
 
-# Procesar cada segmento y generar los subtítulos en formato SRT
+
 for segment in segments:
     start_time = segment.start
     end_time = segment.end
@@ -35,8 +33,12 @@ for segment in segments:
     srt_content += f"{translated_text}\n\n"
     subtitle_count += 1
 
-# Guardar el contenido del archivo SRT en un archivo
-with open("subtitulos.srt", "w", encoding="utf-8") as srt_file:
+
+video_name = os.path.splitext(os.path.basename(video_file))[0]
+srt_file_name = f"{video_name}_sub.srt" # Se crea un archivo SRT con el nombre del video
+
+
+with open(srt_file_name, "w", encoding="utf-8") as srt_file:
     srt_file.write(srt_content)
 
-print("Archivo SRT generado exitosamente.")
+print(f"Archivo SRT generado exitosamente: {srt_file_name}")
